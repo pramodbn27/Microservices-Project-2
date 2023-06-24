0edca961194b2a2514239dd2c9a950fd41cd7b42
pramodbn <begurnagaraj.p@northeastern.edu>
2023-06-24 01:14:25 -0500

    config-api

---
+++ eos-cloud-config-api/src/test/java/com/org/proto/config/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.proto.config.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
