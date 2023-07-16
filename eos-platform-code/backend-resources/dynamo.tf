2874477e98730190d9d185a59fd7021f252df751
pramodbn <begurnagaraj.p@northeastern.edu>
2023-07-16 02:21:50 -0500

    platform-code

---
+++ eos-platform-code/backend-resources/dynamo.tf
@@ -0,0 +1,13 @@
+resource "aws_dynamodb_table" "terraform-lock" {
+    name           = "eos_table"
+    read_capacity  = 5
+    write_capacity = 5
+    hash_key       = "LockID"
+    attribute {
+        name = "LockID"
+        type = "S"
+    }
+    tags = {
+        "Name" = "DynamoDB Terraform State Lock Table"
+    }
+}
